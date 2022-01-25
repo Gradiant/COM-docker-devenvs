3a667ba3a8995f4cd9bc8d1f1ca9f5a3a043e0c1
Andrés Ferreiro González <aferreiro@gradiant.org>
2022-01-25 19:43:40 +0100

    Updates arrayfire installer
    Preious one broke by external deps. New one builds arrayfire from source

--- C++/.devcontainer/scripts/install-arrayfire.sh
+++ C++/.devcontainer/scripts/install-arrayfire.sh
@@ -1,21 +1,82 @@
 #! /usr/bin/env bash
 # This script is intended to install arrayfire library.
-# https://github.com/arrayfire/arrayfire/wiki/Build-Instructions-for-Linux
-# Copyright 2020 Centro Tecnolóxico de Telecomunicacións de Galicia (Gradiant)
+# https://github.com/arrayfire/arrayfire/wiki/Build-Instructions-for-Linux for more options and
+# possible configurations.
+#
+# Copyright 2022 Centro Tecnolóxico de Telecomunicacións de Galicia (Gradiant)
 
-# FIXME 20211104-aferreiro Add installation from source to ensure a specific version gets installed
+ARRAYFIRE_VERSION=3.7.3
 
 if ! [ $(id -u) = 0 ]; then
     echo "The script need to be run as root." >&2
     exit 1
 fi
 
-apt-key adv --fetch-key https://repo.arrayfire.com/GPG-PUB-KEY-ARRAYFIRE-2020.PUB
-echo "deb [arch=amd64] https://repo.arrayfire.com/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/arrayfire.list
+if [ $SUDO_USER ]; then
+    REAL_USER=$SUDO_USER
+else
+    REAL_USER=$(whoami)
+fi
 
-apt-get update
+# Enable exit on error
+set -e
 
-apt-get install -y -qq gnupg2 ca-certificates apt-utils software-properties-common
-apt-get install -y -qq arrayfire
+echo " "
+echo " "
+echo " "
+echo "1. Installing prerequisites"
 
-# echo "---- arrayfire Download  ----"
+apt-get install -y -qq --no-install-recommends build-essential software-properties-common libfreeimage-dev cmake-curses-gui libopenblas-dev libfftw3-dev liblapacke-dev libglfw3-dev libfontconfig1-dev
+
+# TODO 20220124-aferreiro Make Boost installation independent
+sudo -u $REAL_USER wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
+sudo -u $REAL_USER tar xf boost_1_78_0.tar.gz
+cp -r boost_1_78_0/boost /usr/local/include
+
+# # TODO 20220124-aferreiro Make CUDA toolkit installation independent
+# sudo -u $REAL_USER wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+# mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+# add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+# apt-get update
+# apt-get install -y --no-install-recommends -qq cuda
+
+echo " "
+echo " "
+echo " "
+echo "2. Downloading Arrayfire v${ARRAYFIRE_VERSION}"
+
+sudo -u $REAL_USER curl -o arrayfire-${ARRAYFIRE_VERSION}.tar.xz -L https://github.com/arrayfire/arrayfire/releases/download/v${ARRAYFIRE_VERSION}/arrayfire-full-${ARRAYFIRE_VERSION}.tar.bz2
+sudo -u $REAL_USER tar xvf arrayfire-${ARRAYFIRE_VERSION}.tar.xz
+
+cd arrayfire-full-${ARRAYFIRE_VERSION}
+
+echo " "
+echo " "
+echo " "
+echo "3. Building Arrayfire v${ARRAYFIRE_VERSION}"
+
+sudo -u $REAL_USER mkdir build && cd build
+sudo -u $REAL_USER cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DAF_BUILD_EXAMPLES=OFF -DAF_COMPUTE_LIBRARY=BLAS
+
+np=$(nproc)
+
+jobs=$((np / 2))
+
+jobs=$( (($jobs <= 1)) && echo "1" || echo "$jobs")
+
+sudo -u $REAL_USER make -j ${jobs}
+
+echo " "
+echo " "
+echo " "
+echo "4. Installing Arrayfire v${ARRAYFIRE_VERSION}"
+
+make install
+
+ldconfig
+
+echo " "
+echo " "
+echo " "
+echo "Installation complete"
