3392c4d723d37a196823f1a1d1919a5a77a141d2
Andrés Ferreiro González <andresoferreiro@gmail.com>
2022-02-03 18:13:35 +0100

    Fixes error in post_gen_project

--- cpp-dockerfile/hooks/post_gen_project.py
+++ cpp-dockerfile/hooks/post_gen_project.py
@@ -14,53 +14,53 @@ def change_line_endings_crlf_to_lf():
 
 def remove_unused_script_files():
 
-    if "{{ cookiecutter.add_gcc10_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_gcc10_install_script.upper() }}" == "NO":
         LOG.info("Skipping gcc10 installation script file generation ...")
         path = Path("./scripts/install-gcc10.sh")
         path.unlink()
-    if "{{ cookiecutter.add_matplotpp_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_matplotpp_install_script.upper() }}" == "NO":
         LOG.info("Skipping matplotpp installation script file generation ...")
         path = Path("./scripts/install-matplotpp.sh")
         path.unlink()
-    if "{{ cookiecutter.add_catch2_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_catch2_install_script.upper() }}" == "NO":
         LOG.info("Skipping catch2 installation script file generation ...")
         path = Path("./scripts/install-catch2.sh")
         path.unlink()
-    if "{{ cookiecutter.add_spdlog_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_spdlog_install_script.upper() }}" == "NO":
         LOG.info("Skipping spdlog installation script file generation ...")
         path = Path("./scripts/install-spdlog.sh")
         path.unlink()
-    if "{{ cookiecutter.add_json_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_json_install_script.upper() }}" == "NO":
         LOG.info("Skipping json installation script file generation ...")
         path = Path("./scripts/install-json.sh")
         path.unlink()
-    if "{{ cookiecutter.add_armadillo_install_scripts.upper() }}" == "YES":
+    if "{{ cookiecutter.add_armadillo_install_scripts.upper() }}" == "NO":
         LOG.info("Skipping armadillo installation script file generation ...")
         path = Path("./scripts/install-armadillo.sh")
         path.unlink()
         path = Path("./scripts/install-openblas-lapack.sh")
         path.unlink()
-    if "{{ cookiecutter.add_arrrayfire_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_arrrayfire_install_script.upper() }}" == "NO":
         LOG.info("Skipping arrrayfire installation script file generation ...")
         path = Path("./scripts/install-arrrayfire.sh")
         path.unlink()
-    if "{{ cookiecutter.add_itpp_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_itpp_install_script.upper() }}" == "NO":
         LOG.info("Skipping itpp installation script file generation ...")
         path = Path("./scripts/install-itpp.sh")
         path.unlink()
-    if "{{ cookiecutter.add_geographiclib_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_geographiclib_install_script.upper() }}" == "NO":
         LOG.info("Skipping geographiclib installation script file generation ...")
         path = Path("./scripts/install-geographiclib.sh")
         path.unlink()
-    if "{{ cookiecutter.add_pybind11_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_pybind11_install_script.upper() }}" == "NO":
         LOG.info("Skipping pybind11 installation script file generation ...")
         path = Path("./scripts/install-pybind11.sh")
         path.unlink()
-    if "{{ cookiecutter.add_openmp_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_openmp_install_script.upper() }}" == "NO":
         LOG.info("Skipping openmp installation script file generation ...")
         path = Path("./scripts/install-openmp.sh")
         path.unlink()
-    if "{{ cookiecutter.add_ldpc_simd_install_script.upper() }}" == "YES":
+    if "{{ cookiecutter.add_ldpc_simd_install_script.upper() }}" == "NO":
         LOG.info("Skipping ldpc_simd installation script file generation ...")
         path = Path("./scripts/install-ldpc_simd.sh")
         path.unlink()
